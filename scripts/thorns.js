71b4be619c0c5a60982cd906f3953720b7cd4847
Slava0135 <40753025+Slava0135@users.noreply.github.com>
2019-12-29 17:15:03 +0300

    Add files via upload

--- scripts/thorns.js
+++ scripts/thorns.js
@@ -1,6 +1,6 @@
 const sec = 60;
 damage = 10 / sec;
-tileDamage = 10 / sec;
+tileDamage = 100 / sec;
 const thorns = extendContent(ShockMine, "thorns", {
 	unitOn(tile, unit) {
 		unit.damage(damage);
