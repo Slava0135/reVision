94714e6a9d7b7086ce03cca7a6d34303450e3256
Slava0135 <40753025+Slava0135@users.noreply.github.com>
2020-02-12 17:14:59 +0300

    Add files via upload

--- scripts/large-rewall.js
+++ scripts/large-rewall.js
@@ -4,9 +4,8 @@ const cooldown = 60;
 const largeReWall = extendContent(Wall, "large-rewall", {
 	update(tile) {
 		if (tile.entity.health() < tile.entity.maxHealth() & tile.entity.timer.get(0, cooldown)) {
-			if (tile.entity.health() + heal < tile.entity.maxHealth()) {
-				tile.entity.health += heal;
-			} else tile.entity.health = tile.entity.maxHealth();
+			tile.entity.healBy(heal);
+			Effects.effect(Fx.healBlockFull, Tmp.c1.set(Color.valueOf("84f491")), tile.drawx(), tile.drawy(), tile.block().size);
 		}
 	}
 });
