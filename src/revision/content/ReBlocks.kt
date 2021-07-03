7a17b6bc83b81566ec41042d57f8679a591f9513
slava0135 <super.novalskiy_0135@inbox.ru>
2021-07-03 14:32:28 +0300

    apply

--- src/revision/content/ReBlocks.kt
+++ src/revision/content/ReBlocks.kt
@@ -29,240 +29,223 @@ class ReBlocks : ContentList {
 
     override fun load() {
 
-        filter = object : GenericCrafter("filter") {
-            init {
-                requirements(Category.crafting, with(Items.copper, 125, Items.lead, 100, Items.graphite, 50, Items.titanium, 50))
+        filter = object : GenericCrafter("filter") {}.apply {
+            requirements(
+                Category.crafting,
+                with(Items.copper, 125, Items.lead, 100, Items.graphite, 50, Items.titanium, 50)
+            )
 
-                size = 3
-                craftTime = 60f
-                liquidCapacity = 120f
-                craftEffect = Fx.steam
-                ambientSound = release
+            size = 3
+            craftTime = 60f
+            liquidCapacity = 120f
+            craftEffect = Fx.steam
+            ambientSound = release
 
-                hasPower = true
-                hasLiquids = true
-                hasItems = true
+            hasPower = true
+            hasLiquids = true
+            hasItems = true
 
-                outputLiquid = LiquidStack(Liquids.water, 60f)
-                outputItem = ItemStack(Items.thorium, 1)
+            outputLiquid = LiquidStack(Liquids.water, 60f)
+            outputItem = ItemStack(Items.thorium, 1)
 
-                consumes.power(1f)
-                consumes.item(Items.coal, 2)
-                consumes.liquid(ReLiquids.pollutedWater, 1f)
-            }
+            consumes.power(1f)
+            consumes.item(Items.coal, 2)
+            consumes.liquid(ReLiquids.pollutedWater, 1f)
         }
 
-        freezer = object : Freezer("freezer") {
-            init {
-                requirements(Category.crafting, with(Items.copper, 125, Items.lead, 100, Items.graphite, 50, Items.metaglass, 50))
+        freezer = object : Freezer("freezer") {}.apply {
+            requirements(
+                Category.crafting,
+                with(Items.copper, 125, Items.lead, 100, Items.graphite, 50, Items.metaglass, 50)
+            )
 
-                size = 3
-                craftTime = 30f
-                liquidCapacity = 120f
-                craftEffect = Fx.pickup
-                ambientSound = release
+            size = 3
+            craftTime = 30f
+            liquidCapacity = 120f
+            craftEffect = Fx.pickup
+            ambientSound = release
 
-                hasPower = true
-                hasLiquids = true
-                hasItems = true
+            hasPower = true
+            hasLiquids = true
+            hasItems = true
 
-                outputItem = ItemStack(ReItems.snowball, 3)
+            outputItem = ItemStack(ReItems.snowball, 3)
 
-                consumes.power(0.5f)
-                consumes.liquid(Liquids.water, 0.4f)
-            }
+            consumes.power(0.5f)
+            consumes.liquid(Liquids.water, 0.4f)
         }
 
-        heater = object : Heater("heater") {
-            init {
-                requirements(Category.crafting, with(Items.copper, 75, Items.lead, 50, Items.metaglass, 50))
+        heater = object : Heater("heater") {}.apply {
+            requirements(Category.crafting, with(Items.copper, 75, Items.lead, 50, Items.metaglass, 50))
 
-                size = 2
-                craftTime = 20f
-                liquidCapacity = 60f
-                craftEffect = Fx.steam
-                ambientSound = release
+            size = 2
+            craftTime = 20f
+            liquidCapacity = 60f
+            craftEffect = Fx.steam
+            ambientSound = release
 
-                hasPower = true
-                hasLiquids = true
-                hasItems = true
+            hasPower = true
+            hasLiquids = true
+            hasItems = true
 
-                outputLiquid = LiquidStack(Liquids.water, 4f)
+            outputLiquid = LiquidStack(Liquids.water, 4f)
 
-                consumes.power(0.25f)
-                consumes.item(ReItems.snowball, 1)
-            }
+            consumes.power(0.25f)
+            consumes.item(ReItems.snowball, 1)
         }
 
-        largeReWall = object : ReWall("large-rewall") {
-            init {
-                requirements(Category.defense, with(Items.silicon, 12, Items.lead, 8, Items.titanium, 4))
+        largeReWall = object : ReWall("large-rewall") {}.apply {
+            requirements(Category.defense, with(Items.silicon, 12, Items.lead, 8, Items.titanium, 4))
 
-                size = 2
-                update = true
-                health = 2400
-            }
+            size = 2
+            update = true
+            health = 2400
         }
 
-        reWall = object : ReWall("rewall") {
-            init {
-                requirements(Category.defense, with(Items.silicon, 3, Items.lead, 2, Items.titanium, 1))
+        reWall = object : ReWall("rewall") {}.apply {
+            requirements(Category.defense, with(Items.silicon, 3, Items.lead, 2, Items.titanium, 1))
 
-                size = 1
-                update = true
-                health = 600
-            }
+            size = 1
+            update = true
+            health = 600
         }
 
-        scraper = object : Drill("scraper") {
-            init {
-                requirements(Category.production, with(Items.scrap, 4))
+        scraper = object : Drill("scraper") {}.apply {
+            requirements(Category.production, with(Items.scrap, 4))
 
-                size = 1
-                tier = 2
-                drillTime = 600f
-                hasLiquids = false
-                liquidBoostIntensity = 1f
-                drawMineItem = false
-            }
+            size = 1
+            tier = 2
+            drillTime = 600f
+            hasLiquids = false
+            liquidBoostIntensity = 1f
+            drawMineItem = false
         }
 
-        shredder = object : ItemTurret("shredder") {
-            init {
-                requirements(Category.turret, with(Items.scrap, 35))
+        shredder = object : ItemTurret("shredder") {}.apply {
+            requirements(Category.turret, with(Items.scrap, 35))
 
-                ammo(
-                    Items.scrap, ReBullets.scrapShredder,
-                    Items.coal, ReBullets.coalShredder,
-                    Items.silicon, ReBullets.siliconShredder,
-                    ReItems.snowball, ReBullets.snowballShredder
-                )
+            ammo(
+                Items.scrap, ReBullets.scrapShredder,
+                Items.coal, ReBullets.coalShredder,
+                Items.silicon, ReBullets.siliconShredder,
+                ReItems.snowball, ReBullets.snowballShredder
+            )
 
-                size = 1
-                health = 250
-                range = 100f
-                maxAmmo = 30
-                inaccuracy = 30f
-                rotateSpeed = 12f
-                reloadTime = 1f
-            }
+            size = 1
+            health = 250
+            range = 100f
+            maxAmmo = 30
+            inaccuracy = 30f
+            rotateSpeed = 12f
+            reloadTime = 1f
         }
 
-        sieve = object : GenericCrafter("sieve") {
-            init {
-                requirements(Category.crafting, with(Items.copper, 35))
+        sieve = object : GenericCrafter("sieve") {}.apply {
+            requirements(Category.crafting, with(Items.copper, 35))
 
-                size = 1
-                craftTime = 60f
-                craftEffect = Fx.pulverize
+            size = 1
+            craftTime = 60f
+            craftEffect = Fx.pulverize
 
-                hasPower = false
-                hasLiquids = false
-                hasItems = true
+            hasPower = false
+            hasLiquids = false
+            hasItems = true
 
-                outputItem = ItemStack(Items.scrap, 1)
+            outputItem = ItemStack(Items.scrap, 1)
 
-                consumes.item(Items.sand, 4)
-            }
+            consumes.item(Items.sand, 4)
         }
 
-        tainter = object : GenericCrafter("tainter") {
-            init {
-                requirements(Category.crafting, with(Items.copper, 75, Items.lead, 50, Items.titanium, 50))
+        tainter = object : GenericCrafter("tainter") {}.apply {
+            requirements(Category.crafting, with(Items.copper, 75, Items.lead, 50, Items.titanium, 50))
 
-                size = 2
-                craftTime = 60f
-                liquidCapacity = 60f
-                ambientSound = door
-                craftEffect = Fx.pickup
+            size = 2
+            craftTime = 60f
+            liquidCapacity = 60f
+            ambientSound = door
+            craftEffect = Fx.pickup
 
-                hasPower = true
-                hasLiquids = true
-                hasItems = true
+            hasPower = true
+            hasLiquids = true
+            hasItems = true
 
-                outputLiquid = LiquidStack(ReLiquids.pollutedWater, 12f)
+            outputLiquid = LiquidStack(ReLiquids.pollutedWater, 12f)
 
-                consumes.power(0.5f)
-                consumes.item(Items.sporePod, 1)
-                consumes.liquid(Liquids.water, 0.2f)
-            }
+            consumes.power(0.5f)
+            consumes.item(Items.sporePod, 1)
+            consumes.liquid(Liquids.water, 0.2f)
         }
 
-        thorns = object : Thorns("thorns") {
-            init {
-                requirements(Category.effect, with(Items.scrap, 4))
+        thorns = object : Thorns("thorns") {}.apply {
+            requirements(Category.effect, with(Items.scrap, 4))
 
-                hasShadow = false
-                health = 100
-                update = false
-                destructible = true
-                solid = false
-                targetable = false
-            }
+            hasShadow = false
+            health = 100
+            update = false
+            destructible = true
+            solid = false
+            targetable = false
         }
 
-        windTurbine = object : WindTurbine("wind-turbine") {
-            init {
-                requirements(Category.power, with(Items.copper, 35, Items.lead, 35, Items.graphite, 35))
-                size = 2
-                powerProduction = 0.5f
-            }
+        windTurbine = object : WindTurbine("wind-turbine") {}.apply {
+            requirements(Category.power, with(Items.copper, 35, Items.lead, 35, Items.graphite, 35))
+            size = 2
+            powerProduction = 0.5f
+
         }
 
-        collector = object : Collector("collector") {
-            init {
-                requirements(Category.effect, with(Items.titanium, 100))
-                hasItems = true
-                size = 2
-                itemCapacity = 300
-            }
+        collector = object : Collector("collector") {}.apply {
+            requirements(Category.effect, with(Items.titanium, 100))
+            hasItems = true
+            size = 2
+            itemCapacity = 300
         }
 
-        multiDrill = object : MultiDrill("multi-drill") {
-            init {
-                requirements(Category.production, with(Items.copper, 130, Items.silicon, 120, Items.graphite, 100, Items.phaseFabric, 75))
-                size = 4
-                hasPower = true
+        multiDrill = object : MultiDrill("multi-drill") {}.apply {
+            requirements(
+                Category.production,
+                with(Items.copper, 130, Items.silicon, 120, Items.graphite, 100, Items.phaseFabric, 75)
+            )
+            size = 4
+            hasPower = true
 
-                consumes.power(6f)
-                consumes.liquid(Liquids.water, 0.2f).boost()
-            }
+            consumes.power(6f)
+            consumes.liquid(Liquids.water, 0.2f).boost()
         }
 
-        coalLiquefier = object : CoalLiquefier("coal-liquefier") {
-            init {
-                requirements(Category.crafting, with(Items.copper, 150, Items.graphite, 175, Items.lead, 115, Items.thorium, 115, Items.silicon, 75))
-                size = 3
-                craftTime = 60f
-                liquidCapacity = 240f
-                craftEffect = Fx.steam
-                ambientSound = release
+        coalLiquefier = object : CoalLiquefier("coal-liquefier") {}.apply {
+            requirements(
+                Category.crafting,
+                with(Items.copper, 150, Items.graphite, 175, Items.lead, 115, Items.thorium, 115, Items.silicon, 75)
+            )
+            size = 3
+            craftTime = 60f
+            liquidCapacity = 240f
+            craftEffect = Fx.steam
+            ambientSound = release
 
-                outputLiquid = LiquidStack(Liquids.oil, 15f)
+            outputLiquid = LiquidStack(Liquids.oil, 15f)
 
-                hasPower = true
-                hasLiquids = true
-                hasItems = true
+            hasPower = true
+            hasLiquids = true
+            hasItems = true
 
-                consumes.power(4f)
-                consumes.item(Items.coal, 5)
-                consumes.liquid(Liquids.water, 0.25f)
-            }
+            consumes.power(4f)
+            consumes.item(Items.coal, 5)
+            consumes.liquid(Liquids.water, 0.25f)
         }
 
-        hacker = object : HackTurret("hacker") {
-            init {
-                requirements(Category.turret, with(Items.silicon, 120, Items.titanium, 90, Items.graphite, 30))
+        hacker = object : HackTurret("hacker") {}.apply {
+            requirements(Category.turret, with(Items.silicon, 120, Items.titanium, 90, Items.graphite, 30))
 
-                hasPower = true
-                size = 2
-                range = 240f
-                damage = 0.5f
-                health = 160 * size * size
-                rotateSpeed = 10f
+            hasPower = true
+            size = 2
+            range = 240f
+            damage = 0.5f
+            health = 160 * size * size
+            rotateSpeed = 10f
 
-                consumes.powerCond(3f) { e: HackBuild -> e.target != null }
-            }
+            consumes.powerCond(3f) { e: HackTurret.HackBuild -> e.target != null }
         }
     }
 
