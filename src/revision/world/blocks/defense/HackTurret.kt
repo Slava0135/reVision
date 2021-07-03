09e5cb7236b9c18a293749e9d4a336bac59dc324
slava0135 <super.novalskiy_0135@inbox.ru>
2021-07-03 12:53:45 +0300

    ok

--- src/revision/world/blocks/defense/HackTurret.kt
+++ src/revision/world/blocks/defense/HackTurret.kt
@@ -8,7 +8,11 @@ import arc.math.Angles
 import arc.math.Mathf
 import arc.util.io.Reads
 import arc.util.io.Writes
-import mindustry.gen.Sounds
+import mindustry.Vars
+import mindustry.entities.Units
+import mindustry.entities.Units.Sortf
+import mindustry.gen.*
+import mindustry.gen.Unit
 import mindustry.graphics.Drawf
 import mindustry.graphics.Layer
 import mindustry.world.blocks.defense.turrets.BaseTurret
@@ -30,6 +34,8 @@ open class HackTurret(name: String) : BaseTurret(name) {
     var laserColor = Color.white
     var shootSound = Sounds.tractorbeam
     var shootSoundVolume = 0.9f
+
+    var unitSort = Sortf { obj: Unit, x: Float, y: Float -> obj.dst2(x, y) }
 
     init {
         rotateSpeed = 10f
@@ -57,17 +63,63 @@ open class HackTurret(name: String) : BaseTurret(name) {
 
     inner class HackBuild : BaseTurretBuild() {
 
-        var target: Unit? = null
+        var target: Teamc? = null
         var lastX = 0f; var lastY = 0f
-        var any = false
         var progress = 0f;
+
+        override fun updateTile() {
+            if (validateTarget()) {
+
+                if (!Vars.headless) {
+                    Vars.control.sound.loop(shootSound, this, shootSoundVolume)
+                }
+
+                val dest = angleTo(target)
+                rotation = Angles.moveToward(rotation, dest, rotateSpeed * edelta())
+
+                lastX = target!!.x
+                lastY = target!!.y
+
+                if (Angles.within(rotation, dest, shootCone)) {
+                    progress += damage
+                    if (progress > (target as Healthc).maxHealth()) {
+                        target!!.team(team())
+                        reset()
+                    }
+                } else {
+                    reset()
+                }
+            } else {
+                reset()
+                findTarget()
+            }
+        }
+
+        private fun findTarget() {
+            target = Units.bestTarget(team, x, y, range,
+                { e: Unit -> !e.dead() && (e.isGrounded || targetAir) && (!e.isGrounded || targetGround) },
+                { b: Building? -> true }, unitSort
+            )
+        }
+
+        private fun reset() {
+            progress = 0f
+            target = null
+        }
+
+        private fun validateTarget(): Boolean {
+            return !Units.invalidateTarget(target, team, x, y)
+                    && target!!.within(this, range)
+                    && target!!.team() != team
+                    && efficiency() > 0.02f
+        }
 
         override fun draw() {
             Draw.rect(baseRegion, x, y)
             Drawf.shadow(region, x - size / 2f, y - size / 2f, rotation - 90)
             Draw.rect(region, x, y, rotation - 90)
 
-            if (any) {
+            if (target != null) {
                 Draw.z(Layer.bullet)
                 val ang = angleTo(lastX, lastY)
                 Draw.mixcol(laserColor, Mathf.absin(4f, 0.6f))
