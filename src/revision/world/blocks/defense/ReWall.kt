6106757e5e70ddf6e4d5353c70ee4895c86fe16e
slava0135 <super.novalskiy_0135@inbox.ru>
2021-07-29 16:49:31 +0300

    bar

--- src/revision/world/blocks/defense/ReWall.kt
+++ src/revision/world/blocks/defense/ReWall.kt
@@ -1,9 +1,14 @@
 package revision.world.blocks.defense
 
+import arc.Core
+import arc.func.Floatp
+import arc.func.Prov
 import arc.math.Mathf
 import mindustry.Vars
 import mindustry.content.Fx
 import mindustry.graphics.Pal
+import mindustry.ui.Bar
+import mindustry.world.blocks.defense.OverdriveProjector.OverdriveBuild
 import mindustry.world.blocks.defense.Wall
 import mindustry.world.meta.Attribute
 import mindustry.world.meta.Stat
@@ -21,6 +26,18 @@ open class ReWall(name: String) : Wall(name) {
     override fun setStats() {
         super.setStats()
         stats.add(Stat.health, (60f / reload) * (health / fraction), StatUnit.perSecond)
+    }
+
+    override fun setBars() {
+        super.setBars()
+        bars.add(
+            "efficiency"
+        ) { entity: OverdriveBuild ->
+            Bar(
+                { Core.bundle.format("bar.efficiency", (entity.realBoost() * 100).toInt()) },
+                { Pal.accent },
+                { entity.realBoost() })
+        }
     }
 
     inner class ReWallBuild : WallBuild() {
