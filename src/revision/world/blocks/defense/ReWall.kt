14f31b49b7c4a7dacbbc652f246433339cd958c5
slava0135 <super.novalskiy_0135@inbox.ru>
2021-07-29 16:58:05 +0300

    ok

--- src/revision/world/blocks/defense/ReWall.kt
+++ src/revision/world/blocks/defense/ReWall.kt
@@ -1,15 +1,14 @@
 package revision.world.blocks.defense
 
 import arc.Core
-import arc.func.Floatp
-import arc.func.Prov
 import arc.math.Mathf
+import arc.util.Time
 import mindustry.Vars
 import mindustry.content.Fx
 import mindustry.graphics.Pal
 import mindustry.ui.Bar
-import mindustry.world.blocks.defense.OverdriveProjector.OverdriveBuild
 import mindustry.world.blocks.defense.Wall
+import mindustry.world.blocks.production.SolidPump.SolidPumpBuild
 import mindustry.world.meta.Attribute
 import mindustry.world.meta.Stat
 import mindustry.world.meta.StatUnit
@@ -30,26 +29,27 @@ open class ReWall(name: String) : Wall(name) {
 
     override fun setBars() {
         super.setBars()
-        bars.add(
-            "efficiency"
-        ) { entity: OverdriveBuild ->
+        bars.add("efficiency") { entity: ReWallBuild ->
             Bar(
-                { Core.bundle.format("bar.efficiency", (entity.realBoost() * 100).toInt()) },
-                { Pal.accent },
-                { entity.realBoost() })
+                { Core.bundle.formatFloat("bar.efficiency", entity.lastHeal/ Time.delta * 60, 1) },
+                { Pal.ammo },
+                { entity.efficiency() }
+            )
         }
     }
 
     inner class ReWallBuild : WallBuild() {
 
         var charge = 0f
+        var lastHeal = 0f
 
         override fun updateTile() {
             if (this.damaged()) {
                 charge += delta()
                 if (charge > reload) {
                     charge = 0f
-                    heal(efficiency() * maxHealth / fraction)
+                    lastHeal = efficiency() * maxHealth / fraction
+                    heal(lastHeal)
                     Fx.healBlockFull.at(x, y, size.toFloat(), Pal.heal)
                 }
             }
